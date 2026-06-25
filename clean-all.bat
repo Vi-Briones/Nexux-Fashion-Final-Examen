47feec36ab0900f381ba1cbeb0a5c5aeab06dfa3
Bl4z3-more <vi.briones@duocuc.cl>
2026-06-24 21:21:35 -0400

    se agrega docker al proyecto, version 1.0 falta agregar 3 microservicios

---
+++ clean-all.bat
@@ -0,0 +1,23 @@
+cd api-gateway
+call .\mvnw clean
+
+cd ../cliente-service
+call .\mvnw clean
+
+cd ../compra-service
+call .\mvnw clean
+
+cd ../envio-service
+call .\mvnw clean
+
+cd ../inventario-service
+call .\mvnw clean
+
+cd ../producto-service
+call .\mvnw clean
+
+cd ../venta-service
+call .\mvnw clean
+
+cd ../reseña-service
+call .\mvnw clean
